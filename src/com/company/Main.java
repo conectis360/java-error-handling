f31a0cf4d79dd881f75bc072618ad06bcd8cb1dc
James Garcia <james.ramos@gentecontrole.com.br>
2024-10-03 22:48:03 -0300

    feat: basic error handling and call stack learning

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Olá, seja bem-vindo ao teste de Error Handling");
+
+        System.out.println("Digite um numero.");
+
+        Scanner scan = new Scanner(System.in);
+
+        String valor = scan.nextLine();
+        try {
+            if (Integer.parseInt(valor) == 0 || Integer.parseInt(valor) < 0) {
+                throw new Exception("O valor digitado não é válido");
+            }
+
+            System.out.println("O numero digitado: " + Integer.parseInt(valor));
+        }catch (NumberFormatException numberFormatException) {
+            throw new NumberFormatException("Não é possível converter o valor digitado para Integer.");
+        }
+    }
+}
